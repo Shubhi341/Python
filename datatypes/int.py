73c6860674722491ec82faf32f255ac13ac12ff9
Shubhi Patel <shubhi.patel@students.iiit.ac.in>
2026-04-05 19:03:01 +0530

    added int.py and regex_findall

---
+++ datatypes/int.py
@@ -0,0 +1,15 @@
+# Integer variables
+num1=10
+num2=5
+
+# Integer Division
+result1=num1 // num2
+print("integer division", result1)
+
+# Modulus (Remainder)
+result2=num1%num2
+print("modulus : ", result2)
+
+# Absolute Value
+result3=abs(-7)
+print("absolute value", result3)
