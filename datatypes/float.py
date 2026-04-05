4b19a11663c19b16dcc56c41c59c9ea11b08cd0b
Shubhi Patel <shubhi.patel@students.iiit.ac.in>
2026-04-05 18:57:41 +0530

    float datatype

---
+++ datatypes/float.py
@@ -0,0 +1,17 @@
+num1=5.0
+num2=2.5
+
+result1=num1+num2
+print("Addition:", result1)
+
+result2=num1-num2
+print("subtraction:", result2)
+
+result3=num1*num2
+print("Multiplication:", result3)
+
+result4=num1/num2
+print("Division: ", result4)
+
+result5=round(3.14159265359,2)
+print("rounded: ", result5)
