55b04de3ff558aab84c4f48140c1063cdc73144f
Shubhi Patel <shubhi.patel@students.iiit.ac.in>
2026-04-07 23:23:10 +0530

    Assignment for Day_06 completed

---
+++ Day_06/Task2.py
@@ -0,0 +1,10 @@
+
+a=20
+b=4
+
+print("< result",a<b)
+print("> result", a>b)
+print("== result", a==b)
+print(">= result", a>=b)
+print("<= result", a<=b)
+print("!= result", a!=b)
---
+++ Day_06/Task4.py
@@ -0,0 +1,6 @@
+total=10
+total+=2
+total-=3
+total*=10
+total/=5
+print("final total value : ", total)
